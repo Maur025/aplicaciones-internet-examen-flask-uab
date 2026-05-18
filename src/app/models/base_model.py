8c7802ea473793357bdd953308dea1143f19917e
Maur025 <mmoyapalli@gmail.com>
2026-05-18 00:58:49 -0400

    fix: change UUID to string

--- src/app/models/base_model.py
+++ src/app/models/base_model.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column, UUID, Boolean, func, DateTime
+from sqlalchemy import Column, Boolean, func, DateTime, String
 
 from ..extensions import db
 
@@ -8,7 +8,7 @@ from ..extensions import db
 class BaseModel(db.Model):
     __abstract__ = True
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     deleted = Column(Boolean, default=False, nullable=False)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
