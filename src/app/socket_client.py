39c03aa6d1978a765ad1d97866d2c77d31459983
Maur025 <mmoyapalli@gmail.com>
2026-05-24 18:00:57 -0400

    feat: add dinamic register of metrics

--- src/app/socket_client.py
+++ src/app/socket_client.py
@@ -1,6 +1,7 @@
 import socketio
 from flask import Flask
 
+from .services.metric_service import add_metrics
 from .services.server_service import add_servers
 
 WS_CLIENT_HOST = 'http://localhost:9000'
@@ -23,8 +24,28 @@ def disconnect():
 def initialize(data):
     print(data)
     if flask_app:
-        with flask_app.app_context():
-            add_servers(data['servers'])
+        try:
+            with flask_app.app_context():
+                add_servers(data['servers'])
+
+                print('EMITING TO SERVER')
+                sio.emit('trigger:create:metrics', {'message': 'listo'})
+        except Exception as ex:
+            print(f'[SOCKET-CLIENT] Error processing data servers in Flask app context: {ex}')
+
+    else:
+        print('[SOCKET-CLIENT] Flask app context is not available. Cannot process data.')
+
+
+@sio.on('metrics:create')
+def metrics_create_socket(data):
+    if flask_app:
+        try:
+            with flask_app.app_context():
+                add_metrics(data)
+        except Exception as ex:
+            print(f'[SOCKET-CLIENT] Error processing data metrics in Flask app context: {ex}')
+
     else:
         print('[SOCKET-CLIENT] Flask app context is not available. Cannot process data.')
 
